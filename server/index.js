00aee5bb804606fbd566889a4a3c8157c587320c
ubuntu <alma.sorrentino@yahoo.fr>
2022-03-29 14:45:46 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(cors())
 app.use(express.json())
 
 
+var corsOptions = {
+    origin: 'https://thepolytinder.herokuapp.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
 
 //AuthModal
 
